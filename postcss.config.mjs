e3aab1d219911a46c4e8e473cf9f63542ceb8a24
krasi <krasimir.xristov@gmail.com>
2025-01-12 10:32:15 +0100

    refactor PostCSS and Tailwind configuration to streamline plugins and update theme colors and fonts

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,21 +1,6 @@
-module.exports = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-    './app/**/*.{js,ts,jsx,tsx}',
-  ],
-  theme: {
-    extend: {
-      colors: {
-        lightHover: '#fcf4ff',
-        darkHover: '#2a004a',
-        darkTheme: '#11001F',
-      },
-      fontFamily: {
-        Outfit: ['Outfit', 'sans-serif'],
-        Ovo: ['Ovo', 'sans-serif'],
-      },
-    },
+export default {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
   },
-  plugins: [],
-};
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,22 @@
-import type { Config } from "tailwindcss";
-
-export default {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        lightHover: '#fcf4ff',
+        darkHover: '#2a004a',
+        darkTheme: '#11001F',
+      },
+      fontFamily: {
+        Outfit: ['Outfit', 'sans-serif'],
+        Ovo: ['Ovo', 'serif'],
       },
     },
   },
   plugins: [],
-} satisfies Config;
+}

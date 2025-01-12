fc4c3b3af9f424c03f7debc3c1e5444000112279
krasi <krasimir.xristov@gmail.com>
2025-01-12 10:06:49 +0100

    add new colors and fonts

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -3,6 +3,19 @@ const config = {
   plugins: {
     tailwindcss: {},
   },
+  theme: {
+    extend: {
+      colors: {
+        lightHover: '#fcf4ff',
+        darkHover: '#2a004a',
+        darkTheme: '#11001F',
+      },
+      fontFamily: {
+        Outfit: ['Outfit', 'sans-serif'],
+        Ovo: ['Ovo', 'sans-serif'],
+      },
+    },
+  },
 };
 
 export default config;

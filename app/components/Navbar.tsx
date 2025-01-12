1efd66121f18192f890fcce310b2827ba72f5eab
krasi <krasimir.xristov@gmail.com>
2025-01-12 12:17:33 +0100

    add comments in Navbar component

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -4,18 +4,24 @@ import Link from 'next/link'
 import React, { useState } from 'react'
 
 const Navbar: React.FC = () => {
+  // Дефиниране на състоянието за отваряне/затваряне на менюто
   const [isMenuOpen, setIsMenuOpen] = useState(false)
 
+  // Функция за превключване на състоянието на менюто
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen)
   }
 
   return (
     <>
+      {/* Фонова картинка зад навигацията */}
       <div className='fixed top-0 right-0 w-11/12 -z-10 translate-y-[-80%]'>
         <Image src={assets.header_bg_color} alt='' className='w-full' />
       </div>
+
+      {/* Основна навигация */}
       <nav className='w-full fixed px-5 lg:px-8 xl:px-[8%] py-4 flex items-center justify-between z-50'>
+        {/* Лого на сайта */}
         <Link href='/'>
           <Image
             src={assets.logo}
@@ -24,6 +30,7 @@ const Navbar: React.FC = () => {
           />
         </Link>
 
+        {/* Линкове за навигация за десктоп */}
         <ul className='hidden md:flex items-center gap-6 lg:gap-8 rounded-full px-12 py-3 bg-white shadow-sm bg-opacity-50'>
           {navLinks.map((link: NavLink) => (
             <li key={link.href}>
@@ -34,6 +41,7 @@ const Navbar: React.FC = () => {
           ))}
         </ul>
 
+        {/* Допълнителни бутони и линкове */}
         <div className='flex items-center gap-4'>
           <button>
             <Image src={assets.moon_icon} alt='' className='w-6' />
@@ -47,6 +55,7 @@ const Navbar: React.FC = () => {
           </Link>
         </div>
 
+        {/* Бутон за отваряне/затваряне на мобилното меню */}
         <button onClick={toggleMenu} className='md:hidden ml-4'>
           {isMenuOpen ? (
             <Image src={assets.close_black} alt='Close Menu' className='w-6' />
@@ -56,6 +65,7 @@ const Navbar: React.FC = () => {
         </button>
       </nav>
 
+      {/* Полупрозрачен фон зад менюто, когато е отворено */}
       {isMenuOpen && (
         <div
           className='fixed inset-0 bg-black bg-opacity-50 z-40'
@@ -63,14 +73,17 @@ const Navbar: React.FC = () => {
         ></div>
       )}
 
+      {/* Мобилно меню */}
       <div
         className={`fixed top-0 right-0 h-full w-1/2 bg-rosa-50 shadow-lg transform ${
           isMenuOpen ? 'translate-x-0' : 'translate-x-full'
         } transition-transform duration-700 ease-in-out z-50 flex flex-col justify-center items-center`}
       >
+        {/* Бутон за затваряне на мобилното меню */}
         <button onClick={toggleMenu} className='absolute top-4 right-4'>
           <Image src={assets.close_black} alt='Close Menu' className='w-6' />
         </button>
+        {/* Линкове за навигация в мобилното меню */}
         <ul className='flex flex-col items-center'>
           {navLinks.map((link: NavLink) => (
             <li key={link.href} className='mb-4'>

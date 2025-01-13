cc3efaa05e2d7711ecf52b993c55278c931b718c
krasi <krasimir.xristov@gmail.com>
2025-01-13 20:22:00 +0100

    add custom boxShadow options to Tailwind CSS configuration

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -21,6 +21,10 @@ const config: Config = {
         Outfit: ['Outfit', 'sans-serif'],
         Ovo: ['Ovo', 'serif'],
       },
+      boxShadow: {
+        black: '4px 4px 0 #000',
+        white: '4px 4px 0 #fff',
+      },
     },
   },
   plugins: [],

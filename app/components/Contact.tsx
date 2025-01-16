641f55ddd11604945bf6cc058f550c83f58a76be
krasi <krasimir.xristov@gmail.com>
2025-01-16 20:04:22 +0100

    add sending emails funcionality

--- app/components/Contact.tsx
+++ app/components/Contact.tsx
@@ -1,8 +1,32 @@
 import { assets } from '@/assets/assets.data'
 import Image from 'next/image'
-import React from 'react'
+import { FormEvent, useState } from 'react'
 
 const Contact = () => {
+  const [result, setResult] = useState<string>('')
+
+  const onSubmit = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+    setResult('Sending....')
+    const formData = new FormData(event.target as HTMLFormElement)
+
+    formData.append('access_key', process.env.NEXT_PUBLIC_ACCESS_KEY as string)
+
+    const response = await fetch('https://api.web3forms.com/submit', {
+      method: 'POST',
+      body: formData,
+    })
+
+    const data = await response.json()
+
+    if (data.success) {
+      setResult('Form Submitted Successfully' as string)
+      ;(event.target as HTMLFormElement).reset()
+    } else {
+      console.log('Error', data)
+      setResult(data.message)
+    }
+  }
   return (
     <div
       id='contact'
@@ -18,19 +42,21 @@ const Contact = () => {
         dolorum quia quos aperiam.
       </p>
 
-      <form className='max-w-2xl mx-auto'>
+      <form onSubmit={onSubmit} className='max-w-2xl mx-auto'>
         <div className='grid grid-cols-auto gap-6 mt-10 mb-8'>
           <input
             type='text'
             placeholder='Enter your name'
             required
             className='flex-1 p-3 outline-none border-[0.5px] border-gray-400 rounded-md bg-white'
+            name='name'
           />
           <input
             type='email'
             placeholder='Enter your email'
             required
             className='flex-1 p-3 outline-none border-[0.5px] border-gray-400 rounded-md bg-white'
+            name='email'
           />
         </div>
         <textarea
@@ -38,6 +64,7 @@ const Contact = () => {
           placeholder='Enter your message'
           required
           className='w-full p-4 outline-none border-[0.5px] border-gray-400 rounded-md bg-white mb-6'
+          name='message'
         ></textarea>
 
         <button
@@ -48,7 +75,7 @@ const Contact = () => {
           <Image src={assets.right_arrow_white} alt='arrow' className='w-4' />
         </button>
 
-        <p className='mt-4'>sending...</p>
+        <p className='mt-4'>{result}</p>
       </form>
     </div>
   )

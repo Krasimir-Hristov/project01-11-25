fba81ea80f9333abc4647a4d907832f5529999c6
krasi <krasimir.xristov@gmail.com>
2025-01-20 19:10:32 +0100

    Add animation on about component

--- app/components/About.tsx
+++ app/components/About.tsx
@@ -1,26 +1,62 @@
 import { assets, InfoItem, infoList, toolsData } from '@/assets/assets.data'
 import Image from 'next/image'
 import React from 'react'
+import { motion } from 'motion/react'
 import { DarkModeProps } from '../types/darkMode'
 
 const About = ({ isDarkMode }: DarkModeProps) => {
   return (
-    <div className='w-full px-[12%] py-10 scroll-mt-20'>
+    <motion.div
+      id='about'
+      className='w-full px-[12%] py-10 scroll-mt-20'
+      initial={{ opacity: 0 }}
+      whileInView={{ opacity: 1 }}
+      transition={{ duration: 1 }}
+    >
       {/* Introduction section */}
-      <h4 className='text-center mb-2 text-lg font-Ovo'>Introduction</h4>
-      <h2 className='text-center text-5xl font-Ovo'>About Us</h2>
+      <motion.h4
+        initial={{ y: -20, opacity: 0 }}
+        whileInView={{ y: 0, opacity: 1 }}
+        transition={{ duration: 0.5, delay: 0.3 }}
+        className='text-center mb-2 text-lg font-Ovo'
+      >
+        Introduction
+      </motion.h4>
+      <motion.h2
+        initial={{ y: -20, opacity: 0 }}
+        whileInView={{ y: 0, opacity: 1 }}
+        transition={{ duration: 0.5, delay: 0.5 }}
+        className='text-center text-5xl font-Ovo'
+      >
+        About Us
+      </motion.h2>
 
       {/* Main content section */}
-      <div className='flex w-full max-w-7xl mx-auto flex-col lg:flex-row items-center gap-20 my-20 justify-center'>
+      <motion.div
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        transition={{ duration: 0.8 }}
+        className='flex w-full max-w-7xl mx-auto flex-col lg:flex-row items-center gap-20 my-20 justify-center'
+      >
         {/* Company image */}
-        <div className='w-64 sm:w-80 rounded-3xl max-w-none'>
+        <motion.div
+          initial={{ opacity: 0, scale: 0.9 }}
+          whileInView={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 0.6 }}
+          className='w-64 sm:w-80 rounded-3xl max-w-none'
+        >
           <Image
             src={assets.user_image}
             alt='company'
             className='w-full rounded-3xl'
           />
-        </div>
-        <div className='flex-1'>
+        </motion.div>
+        <motion.div
+          initial={{ opacity: 0 }}
+          whileInView={{ opacity: 1 }}
+          transition={{ duration: 0.6, delay: 0.8 }}
+          className='flex-1'
+        >
           {/* Company description */}
           <p className='mb-10 max-w-2xl font-Ovo text-center lg:text-left'>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
@@ -30,13 +66,19 @@ const About = ({ isDarkMode }: DarkModeProps) => {
           </p>
 
           {/* Info list section */}
-          <ul className='grid grid-cols-1 sm:grid-cols-3 gap-6 max-w-2xl mx-auto lg:mx-0'>
+          <motion.ul
+            initial={{ opacity: 0 }}
+            whileInView={{ opacity: 1 }}
+            transition={{ duration: 0.8, delay: 1 }}
+            className='grid grid-cols-1 sm:grid-cols-3 gap-6 max-w-2xl mx-auto lg:mx-0'
+          >
             {infoList.map(
               (
                 { icon, title, iconDark, description }: InfoItem,
                 index: number
               ) => (
-                <li
+                <motion.li
+                  whileHover={{ scale: 1.05 }}
                   key={index}
                   className='border-[0.5px] border-gray-400 rounded-xl p-6 cursor-pointer hover:bg-lightHover hover:-translate-y-1 duration-500 hover:shadow-black dark:border-white dark:hover:shadow-white dark:hover:bg-darkHover/50'
                 >
@@ -54,31 +96,42 @@ const About = ({ isDarkMode }: DarkModeProps) => {
                   <p className='text-gray-600 text-sm dark:text-white/80'>
                     {description}
                   </p>
-                </li>
+                </motion.li>
               )
             )}
-          </ul>
+          </motion.ul>
 
           {/* Tools section */}
-          <h4 className='my-6 text-gray-700 font-Ovo dark:text-white/80 text-center lg:text-left'>
+          <motion.h4
+            initial={{ y: 20, opacity: 0 }}
+            whileInView={{ y: 0, opacity: 1 }}
+            transition={{ duration: 0.5, delay: 1.3 }}
+            className='my-6 text-gray-700 font-Ovo dark:text-white/80 text-center lg:text-left'
+          >
             Tools we use
-          </h4>
+          </motion.h4>
 
           {/* Tools list */}
-          <ul className='flex items-center gap-3 sm:gap-5 justify-center lg:justify-start'>
+          <motion.ul
+            initial={{ opacity: 0 }}
+            whileInView={{ opacity: 1 }}
+            transition={{ duration: 0.6, delay: 1.5 }}
+            className='flex items-center gap-3 sm:gap-5 justify-center lg:justify-start'
+          >
             {toolsData.map((tool, index) => (
-              <li
+              <motion.li
+                whileHover={{ scale: 1.1 }}
                 key={index}
                 className='flex items-center justify-center w-12 sm:w-14 aspect-square border border-gray-400 rounded-xl cursor-pointer hover:-translate-y-1 duration-500'
               >
                 {/* Tool icon */}
                 <Image src={tool} alt={String(tool)} className='w-5 sm:w-7' />
-              </li>
+              </motion.li>
             ))}
-          </ul>
-        </div>
-      </div>
-    </div>
+          </motion.ul>
+        </motion.div>
+      </motion.div>
+    </motion.div>
   )
 }
 

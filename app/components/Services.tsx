bcb4e0582b10a6c34847814b085cc13653672177
krasi <krasimir.xristov@gmail.com>
2025-01-20 19:23:20 +0100

    add animation on Work component

--- app/components/Services.tsx
+++ app/components/Services.tsx
@@ -3,30 +3,62 @@ import { assets, ServiceData, serviceData } from '@/assets/assets.data'
 import Image from 'next/image' // Import the Image component from next/image
 import Link from 'next/link' // Import the Link component from next/link
 import React from 'react' // Import the React library
+import { motion } from 'motion/react'
 
 // Define the Services component as a functional component
 const Services = () => {
   return (
     // Container element for the services section
-    <div id='services' className='w-full px-[12%] py-10 scroll-mt-20'>
+    <motion.div
+      initial={{ opacity: 0 }}
+      whileInView={{ opacity: 1 }}
+      transition={{ duration: 1 }}
+      id='services'
+      className='w-full px-[12%] py-10 scroll-mt-20'
+    >
       {/* // Subheading for the services section */}
-      <h4 className='text-center mb-2 text-lg font-Ovo'>What we offer?</h4>
+      <motion.h4
+        initial={{ y: 20, opacity: 0 }}
+        whileInView={{ y: 0, opacity: 1 }}
+        transition={{ duration: 0.5, delay: 0.3 }}
+        className='text-center mb-2 text-lg font-Ovo'
+      >
+        What we offer?
+      </motion.h4>
       {/* // Main heading for the services section */}
-      <h2 className='text-center text-5xl font-Ovo'>Our Services</h2>
+      <motion.h2
+        initial={{ y: -20, opacity: 0 }}
+        whileInView={{ y: 0, opacity: 1 }}
+        transition={{ duration: 0.5, delay: 0.5 }}
+        className='text-center text-5xl font-Ovo'
+      >
+        Our Services
+      </motion.h2>
       {/* // Brief description of the services offered */}
-      <p className='text-center max-w-2xl mx-auto mt-5 mb-12 font-Ovo'>
+      <motion.p
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        transition={{ duration: 0.7 }}
+        className='text-center max-w-2xl mx-auto mt-5 mb-12 font-Ovo'
+      >
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
         voluptatem, quod, quas, doloremque quae nemo quibusdam voluptates
         dolorum quia quos aperiam.
-      </p>
+      </motion.p>
       {/* // Grid container for the individual service cards */}
-      <div className='grid grid-cols-auto gap-6 my-10'>
+      <motion.div
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        transition={{ duration: 0.6, delay: 0.9 }}
+        className='grid grid-cols-auto gap-6 my-10'
+      >
         {/* // Map through the serviceData array and render a service card for each
         item */}
         {serviceData.map(
           ({ icon, title, description, link }: ServiceData, index: number) => (
             // Individual service card container
-            <div
+            <motion.div
+              whileHover={{ scale: 1.05 }}
               key={index}
               className='border border-gray-400 rounded-lg px-8 py-12 hover:shadow-black cursor-pointer
                hover:bg-lightHover hover:-translate-x-1 duration-500 dark:hover:bg-darkHover dark:hover:shadow-white'
@@ -50,11 +82,11 @@ const Services = () => {
                 Read More
                 <Image src={assets.right_arrow} alt='arrow' className='w-4' />
               </Link>
-            </div>
+            </motion.div>
           )
         )}
-      </div>
-    </div>
+      </motion.div>
+    </motion.div>
   )
 }
 

16abce2d4428291b9063c2345059a47f78da7786
krasi <krasimir.xristov@gmail.com>
2025-02-17 20:50:31 +0100

    add margin-bottom to header title for improved spacing

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -41,7 +41,7 @@ const Header = () => {
         initial={{ y: -20, opacity: 0 }}
         whileInView={{ y: 0, opacity: 1 }}
         transition={{ duration: 0.8, delay: 0.5 }}
-        className='text-3xl sm:text-6xl lg:text-[66px] font-Ovo'
+        className='text-3xl sm:text-6xl lg:text-[66px] font-Ovo mb-6'
       >
         Building Modern Websites based in Germany!
       </motion.h1>
